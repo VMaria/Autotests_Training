2fbd05e10df8673d95daeb17034a34632a3507f2
VMaria <mariaviktorenko@gmail.com>
2016-10-23 10:55:19 +0700

    Добавлен метод расчета расстояния между двумя точками на плоскости

--- sandbox/src/main/java/ru/stqa/training/sandbox/DistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/DistanceBetweenPoints.java
@@ -1,16 +1,9 @@
 package ru.stqa.training.sandbox;
 
 public class DistanceBetweenPoints {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         Point p1 = new Point(6,7);
         Point p2 = new Point(3,3);
-
-        System.out.println(distance(p1,p2));
-    }
-    public static double distance(Point p1,Point p2) {
-        double x = p1.x-p2.x;
-        double y = p1.y-p2.y;
-        return Math.sqrt(x*x + y*y);
+        System.out.println("Растстояние между точками равно "+p1.distance(p2));
     }
 }
--- sandbox/src/main/java/ru/stqa/training/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/training/sandbox/Point.java
@@ -4,8 +4,14 @@ public class Point {
     public double x;
     public double y;
 
-    public Point (double x, double y){
-        this.x = x;
-        this.y = y;
+
+    public Point(double a, double b) {
+        x = a;
+        y = b;
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt( Math.pow(p.x-x,2) + Math.pow(p.y-y,2));
     }
 }
+
