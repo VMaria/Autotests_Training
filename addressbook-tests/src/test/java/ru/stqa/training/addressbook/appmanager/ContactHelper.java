bce51d81a577e666745b543e681c826658b51497
VMaria <mariaviktorenko@gmail.com>
2016-10-28 19:44:46 +0700

    Добавлены классы-помощники специализированные классы-помощники NavigationHelper, GroupHelper, ContactHelper, SessionHelper, HelperBase

---
+++ addressbook-tests/src/test/java/ru/stqa/training/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,59 @@
+package ru.stqa.training.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.training.addressbook.model.ContactData;
+
+public class ContactHelper extends HelperBase {
+
+
+    public ContactHelper(FirefoxDriver wd) {
+
+        super(wd);
+    }
+
+    public void submitContactCreation() {
+
+        click(By.xpath("//div[@id='content']/form/input[21]"));
+    }
+
+    public void fillContactForm(ContactData contactData) {
+
+        type(By.name("firstname"),contactData.getFname());
+        type(By.name("lastname"),contactData.getLname());
+        type(By.name("title"),contactData.getTitle());
+        type(By.name("address"),contactData.getAddress());
+        click(By.xpath(contactData.getDay()));
+        click(By.xpath(contactData.getMonth()));
+        type(By.name("byear"),contactData.getYear());
+    }
+
+    public void initContactCreation() {
+
+        click(By.linkText("add new"));
+    }
+    public void initContactModification() {
+
+        click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+    }
+
+    public void submitContactModification() {
+
+        click(By.name("update"));
+    }
+
+    public void selectContact() {
+
+        click(By.xpath("//div/div[4]/form[2]/table/tbody/tr[2]/td[1]/input"));
+    }
+
+    public void deleteSelectedContact() {
+
+        click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
+    }
+
+    public void closeDialogWindow () {
+
+        wd.switchTo().alert().accept();
+    }
+}
---
+++ addressbook-tests/src/test/java/ru/stqa/training/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,22 @@
+package ru.stqa.training.addressbook.appmanager;
+
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigationHelper extends HelperBase {
+
+    public NavigationHelper(FirefoxDriver wd) {
+
+        super (wd);
+    }
+
+    public void gotoGroupPage() {
+
+        click(By.linkText("groups"));
+    }
+
+    public void gotoHomePage() {
+
+        click(By.linkText("home page"));
+    }
+}
