0b4f3c9358bb18012c530dfb669a7a07e8a080e3
VMaria <mariaviktorenko@gmail.com>
2016-11-01 19:29:58 +0700

    Рефакторинг

--- addressbook-tests/src/test/java/ru/stqa/training/addressbook/appmanager/NavigationHelper.java
+++ addressbook-tests/src/test/java/ru/stqa/training/addressbook/appmanager/NavigationHelper.java
@@ -1,8 +1,7 @@
 package ru.stqa.training.addressbook.appmanager;
 
-        import org.openqa.selenium.By;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends HelperBase {
 
@@ -10,14 +9,20 @@ public class NavigationHelper extends HelperBase {
 
         super (wd);
     }
-
     public void gotoGroupPage() {
-
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        }
         click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
 
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home page"));
     }
 }
