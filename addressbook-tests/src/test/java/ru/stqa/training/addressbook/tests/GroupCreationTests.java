2ffc48c684f669bedc28e9025e1e6dae0d92cbdd
Maria Viktorenko <mariaviktorenko@gmail.com>
2016-11-17 16:39:30 +0700

    Рефакторинг создания группы - Hamcrest

--- addressbook-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-tests/src/test/java/ru/stqa/training/addressbook/tests/GroupCreationTests.java
@@ -1,12 +1,11 @@
 package ru.stqa.training.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.training.addressbook.model.GroupData;
+import ru.stqa.training.addressbook.model.Groups;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
@@ -15,20 +14,15 @@ public class GroupCreationTests extends TestBase {
 
         app.goTo().groupPage();
         app.group().isThereAGroup();
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData group = new GroupData().withName("test1");
         app.group().create(group);
         app.goTo().groupPage();
-        Set<GroupData> after = app.group().all();
-        Assert.assertEquals(after.size(), before.size() + 1);
-
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-        before.add(group);
-        //Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        //before.sort(byId);
-        //after.sort(byId);
+        Groups after = app.group().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
         System.out.println(after);
         System.out.println(before);
-        Assert.assertEquals(before, after);
+        assertThat(after, equalTo(
+                before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
