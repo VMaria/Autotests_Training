893829420257742df994228386ebd91cebadf0d0
Maria Viktorenko <mariaviktorenko@gmail.com>
2016-11-23 15:35:14 +0700

    Добавлена загрузка тестовых данных из json для создания групп

--- addressbook-tests/src/test/java/ru/stqa/training/addressbook/generators/GroupDataGenerator.java
+++ addressbook-tests/src/test/java/ru/stqa/training/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package ru.stqa.training.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.training.addressbook.model.GroupData;
 
@@ -42,9 +44,19 @@ public class GroupDataGenerator {
             saveAsCsv(groups, new File(file));
         } else if (dataformat.equals("xml")) {
             saveAsXml(groups, new File(file));
+        } else if (dataformat.equals("json")) {
+            saveAsJson(groups, new File(file));
         } else {
             System.out.println("Unrecognized format " + dataformat);
         }
+    }
+
+    private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
     }
 
     private void saveAsXml(List<GroupData> groups, File file) throws IOException {
@@ -52,7 +64,7 @@ public class GroupDataGenerator {
         xstream.processAnnotations(GroupData.class);
         String xml = xstream.toXML(groups);
         Writer writer = new FileWriter(file);
-        writer.write(xml);;
+        writer.write(xml);
         writer.close();
     }
 
