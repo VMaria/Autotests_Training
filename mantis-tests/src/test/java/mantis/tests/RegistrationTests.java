10c6e76b899df40cbc40a1376ecb246f48a9eff0
Maria Viktorenko <mariaviktorenko@gmail.com>
2016-12-08 15:07:00 +0700

    Добавлен почтовый сервер. Дописан тест регистрации

--- mantis-tests/src/test/java/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/mantis/tests/RegistrationTests.java
@@ -1,14 +1,46 @@
 package mantis.tests;
 
 
+import mantis.model.MailMessage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import mantis.tests.TestBase;
+import ru.lanwen.verbalregex.VerbalExpression;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class RegistrationTests extends TestBase {
 
-    @Test
-    public void testRegistation() {
-        TestBase.app.registration().start("user1", "test@test.com");
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
 
+    @Test
+    public void testRegistration() throws IOException {
+        long now = System.currentTimeMillis();
+        String email = "user" + now + "@test.com";
+        String user = "user" + now;
+        String password = "password";
+        TestBase.app.registration().start(user, email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(user, password));
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
     }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -5,3 +5,5 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
+$g_phpmailer method = PHPMAILER_METHOD_SMPT;
+$g_smpt_host = 'localhost';
